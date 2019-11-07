49788e8afe4c750962971fae3b9ba92b98e4c32e
Cory Hoban <hobancor@gmail.com>
2019-11-07 09:50:21 -0500

    Add cube styleing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var cubex = -22,    // initial rotation
+cubey = -38,
+cubez = 0;
+function rotate(variableName, degrees) {
+  console.log(variableName);
+    window[variableName] = window[variableName] + degrees;
+    rotCube(cubex, cubey, cubez);
+}
+function rotCube(degx, degy, degz){
+    let segs = "rotateX("+degx+"deg) rotateY("+degy+"deg) rotateZ("+degz+"deg) translateX(0) translateY(0) translateZ(0)";
+    console.log(segs);
+    document.getElementById("D3Cube").style.transform = `translate(${segs})`
+}
+function turnRight() {
+    rotate("cubey", 90);
+}
+function turnLeft() {
+    rotate("cubey", -90);
+}
+function flipCube() {
+    rotate("cubez", -180);
+}
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +32,22 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="wrapD3Cube">
+          <div id="D3Cube">
+            <div id="side1">Words on this side</div>
+            <div id="side2">Words on this side</div>
+            <div id="side3">Words on this side</div>
+            <div id="side4">Words on this side</div>
+            <div id="side5">Words on this side</div>
+            <div id="side6">Words on this side</div>
+          </div>
+        </div>
+
+        <div>
+          <div onClick={() => turnLeft()}>Left</div>
+          <div onClick={() => turnRight()}>Right</div> <br />
+          <div onClick={() => flipCube()}>Flip</div>
+        </div>
       </header>
     </div>
   );
