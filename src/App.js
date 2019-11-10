eb9aab6266a91ca9998ed338f048cbffe35bec5f
Jordan Rhodes <jsrhodes15@gmail.com>
2019-11-10 08:28:05 -0500

    Initial prettier run, format codebase. Prettier call in git hook only formats files that have changed.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
-import React from 'react';
-import $ from 'jquery';
+import React from "react";
+import $ from "jquery";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
-import './App.scss';
-import './assets/sass/main.scss'
+import "./App.scss";
+import "./assets/sass/main.scss";
 
 function App() {
-  var cubex = -22,    // initial rotation
-cubey = -10,
-cubez = 0;
-function rotate(variableName, degrees) {
-    if (variableName === 'cubey') {
+  var cubex = -22, // initial rotation
+    cubey = -10,
+    cubez = 0;
+  function rotate(variableName, degrees) {
+    if (variableName === "cubey") {
       cubey += degrees;
-    } else if (variableName === 'cubez') {
+    } else if (variableName === "cubez") {
       cubez += degrees;
-    } 
+    }
     window[variableName] = window[variableName] + degrees;
     rotCube(cubex, cubey, cubez);
-}
-function rotCube(degx, degy, degz){
-    let segs = "rotateX("+degx+"deg) rotateY("+degy+"deg) rotateZ("+degz+"deg) translateX(0) translateY(0) translateZ(0)";
-    $('#D3Cube').css({"transform":segs});
-}
-function turnRight() {
+  }
+  function rotCube(degx, degy, degz) {
+    let segs =
+      "rotateX(" +
+      degx +
+      "deg) rotateY(" +
+      degy +
+      "deg) rotateZ(" +
+      degz +
+      "deg) translateX(0) translateY(0) translateZ(0)";
+    $("#D3Cube").css({ transform: segs });
+  }
+  function turnRight() {
     rotate("cubey", 90);
-}
-function turnLeft() {
+  }
+  function turnLeft() {
     rotate("cubey", -90);
-}
-function flipCube() {
+  }
+  function flipCube() {
     rotate("cubez", -180);
-}
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the intro page
-        </p>
+        <p>This is the intro page</p>
         <div id="wrapD3Cube">
           <div id="D3Cube">
             <div id="side1">Words on this side</div>
@@ -51,9 +56,9 @@ function flipCube() {
         </div>
 
         <p>
-            <a onClick={() => turnLeft()}>Left</a>
-            <a onClick={() => turnRight()}>Right</a> <br />
-            <a onClick={() => flipCube()}>Flip</a>
+          <a onClick={() => turnLeft()}>Left</a>
+          <a onClick={() => turnRight()}>Right</a> <br />
+          <a onClick={() => flipCube()}>Flip</a>
         </p>
       </header>
     </div>
